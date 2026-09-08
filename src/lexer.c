8ed984abc2790a730724d05b6d9b09e36db3ba53
dokuzsertkol <emreeyupoglu009@gmail.com>
2026-09-08 23:16:31 +0300

    lexer: new data types

--- src/lexer.c
+++ src/lexer.c
@@ -46,7 +46,75 @@ Token lexer_next_token(Lexer *lexer) {
 
     char c = *token.start;
 
-    // string lexer
+    // utf-16 or utf-32 char and string lexer
+    if (c == 'u' || c == 'U') {
+        if (lexer->current[1] == '\'' || lexer->current[1] == '"') {
+            char quote = lexer->current[1];
+
+            token.length = 2;
+
+            if (quote == '\'') {
+                token.type = (c == 'u') ? TOKEN_C2_LITERAL : TOKEN_C4_LITERAL;
+            } else {
+                token.type = (c == 'u') ? TOKEN_S2_LITERAL : TOKEN_S4_LITERAL;
+            }
+
+            lexer->current += 2;
+
+            while (*lexer->current != quote &&
+                *lexer->current != '\0') {
+
+                if (*lexer->current == '\\' &&
+                    lexer->current[1] != '\0') {
+                    lexer->current += 2;
+                } else {
+                    lexer->current++;
+                }
+            }
+
+            if (*lexer->current == '\0') {
+                token.type = TOKEN_ERROR;
+                return token;
+            }
+
+            lexer->current++;
+
+            token.length = lexer->current - token.start;
+
+            return token;
+        }
+    }
+
+    // UTF-8 char and string lexer
+    if (c == '"' || c == '\'') {
+        char quote = c;
+
+        token.type = (quote == '"') ? TOKEN_S1_LITERAL : TOKEN_C1_LITERAL;
+
+        lexer->current++;
+
+        while (*lexer->current != quote &&
+            *lexer->current != '\0') {
+
+            if (*lexer->current == '\\' && lexer->current[1] != '\0') {
+                lexer->current += 2;
+            } else {
+                lexer->current++;
+            }
+        }
+
+        if (*lexer->current == '\0') {
+            token.type = TOKEN_ERROR;
+            return token;
+        }
+
+        lexer->current++;
+
+        token.length = lexer->current - token.start;
+
+        return token;
+    }
+    // identifier and keyword lexer
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
         while ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')) {
             c = *(token.start + ++token.length);
@@ -54,6 +122,51 @@ Token lexer_next_token(Lexer *lexer) {
 
         if (token.length == 2 && strncmp(token.start, "if", 2) == 0) {
             token.type = TOKEN_IF;
+        }
+        else if (token.length == 2 && strncmp(token.start, "i1", 2) == 0) {
+            token.type = TOKEN_I1;
+        }
+        else if (token.length == 2 && strncmp(token.start, "i2", 2) == 0) {
+            token.type = TOKEN_I2;
+        }
+        else if (token.length == 2 && strncmp(token.start, "i4", 2) == 0) {
+            token.type = TOKEN_I4;
+        }
+        else if (token.length == 2 && strncmp(token.start, "i8", 2) == 0) {
+            token.type = TOKEN_I8;
+        }
+        else if (token.length == 2 && strncmp(token.start, "u1", 2) == 0) {
+            token.type = TOKEN_U1;
+        }
+        else if (token.length == 2 && strncmp(token.start, "u2", 2) == 0) {
+            token.type = TOKEN_U2;
+        }
+        else if (token.length == 2 && strncmp(token.start, "u4", 2) == 0) {
+            token.type = TOKEN_U4;
+        }
+        else if (token.length == 2 && strncmp(token.start, "u8", 2) == 0) {
+            token.type = TOKEN_U8;
+        }
+        else if (token.length == 2 && strncmp(token.start, "f4", 2) == 0) {
+            token.type = TOKEN_F4;
+        }
+        else if (token.length == 2 && strncmp(token.start, "f8", 2) == 0) {
+            token.type = TOKEN_F8;
+        }
+        else if (token.length == 2 && strncmp(token.start, "c1", 2) == 0) {
+            token.type = TOKEN_C1;
+        }
+        else if (token.length == 2 && strncmp(token.start, "c2", 2) == 0) {
+            token.type = TOKEN_C2;
+        }
+        else if (token.length == 2 && strncmp(token.start, "c4", 2) == 0) {
+            token.type = TOKEN_C4;
+        }
+        else if (token.length == 2 && strncmp(token.start, "b1", 2) == 0) {
+            token.type = TOKEN_B1;
+        }
+        else if (token.length == 2 && strncmp(token.start, "v0", 2) == 0) {
+            token.type = TOKEN_V0;
         }
         else if (token.length == 2 && strncmp(token.start, "sp", 2) == 0) {
             token.type = TOKEN_SP;
@@ -67,18 +180,6 @@ Token lexer_next_token(Lexer *lexer) {
         else if (token.length == 2 && strncmp(token.start, "hp", 2) == 0) {
             token.type = TOKEN_HP;
         }
-        else if (token.length == 3 && strncmp(token.start, "int", 3) == 0) {
-            token.type = TOKEN_INT;
-        }
-        else if (token.length == 4 && strncmp(token.start, "char", 4) == 0) {
-            token.type = TOKEN_CHAR;
-        }
-        else if (token.length == 4 && strncmp(token.start, "bool", 4) == 0) {
-            token.type = TOKEN_BOOL;
-        }
-        else if (token.length == 4 && strncmp(token.start, "void", 4) == 0) {
-            token.type = TOKEN_VOID;
-        }
         else if (token.length == 4 && strncmp(token.start, "else", 4) == 0) {
             token.type = TOKEN_ELSE;
         }
@@ -87,9 +188,6 @@ Token lexer_next_token(Lexer *lexer) {
         }
         else if (token.length == 5 && strncmp(token.start, "false", 5) == 0) {
             token.type = TOKEN_FALSE;
-        }
-        else if (token.length == 5 && strncmp(token.start, "float", 5) == 0) {
-            token.type = TOKEN_FLOAT;
         }
         else if (token.length == 5 && strncmp(token.start, "while", 5) == 0) {
             token.type = TOKEN_WHILE;
@@ -121,40 +219,11 @@ Token lexer_next_token(Lexer *lexer) {
         while ((c >= '0' && c <= '9') || (c == '.')) {
             if (c == '.') {
                 if (!dotCount++) { token.type = TOKEN_ERROR; break; }
-                token.type = TOKEN_FLOAT;
+                token.type = TOKEN_FLOAT_LITERAL;
             }
             token.length++;
             c = *(token.start + token.length);
         }
-        lexer->current += token.length;
-        return token;
-    }
-
-    // char lexer
-    if (c == '\'') {
-         // normal char
-        if (lexer->current[1] != '\\') {
-            if (lexer->current[1] == '\0' || lexer->current[2] != '\'') {
-                token.type = TOKEN_ERROR;
-                return token;
-            }
-
-            token.type = TOKEN_CHAR_LITERAL;
-            token.length = 3;
-
-            lexer->current += token.length;
-            return token;
-        }
-
-        // escaped char
-        if (lexer->current[2] == '\0' || lexer->current[3] != '\'') {
-            token.type = TOKEN_ERROR;
-            return token;
-        }
-
-        token.type = TOKEN_CHAR_LITERAL;
-        token.length = 4;
-
         lexer->current += token.length;
         return token;
     }
@@ -282,11 +351,6 @@ void lexer_print(const Lexer *lexer) {
             case TOKEN_BAR_BAR: printf("BAR_BAR"); break;
             case TOKEN_EXCLAM: printf("EXCLAM"); break;
 
-            case TOKEN_INT: printf("INT"); break;
-            case TOKEN_CHAR: printf("CHAR"); break;
-            case TOKEN_FLOAT: printf("FLOAT"); break;
-            case TOKEN_BOOL: printf("BOOL"); break;
-            case TOKEN_VOID: printf("VOID"); break;
             case TOKEN_RETURN: printf("RETURN"); break;
             case TOKEN_IF: printf("IF"); break;
             case TOKEN_ELSE: printf("ELSE"); break;
@@ -295,9 +359,31 @@ void lexer_print(const Lexer *lexer) {
             case TOKEN_CONTINUE: printf("CONTINUE"); break;
             case TOKEN_STRUCT: printf("STRUCT"); break;
 
+
+            case TOKEN_I1: printf("I1"); break;
+            case TOKEN_I2: printf("I2"); break;
+            case TOKEN_I4: printf("I4"); break;
+            case TOKEN_I8: printf("I8"); break;
+            case TOKEN_U1: printf("U1"); break;
+            case TOKEN_U2: printf("U2"); break;
+            case TOKEN_U4: printf("U4"); break;
+            case TOKEN_U8: printf("U8"); break;
+            case TOKEN_C1: printf("C1"); break;
+            case TOKEN_C2: printf("C2"); break;
+            case TOKEN_C4: printf("C4"); break;
+            case TOKEN_F4: printf("F4"); break;
+            case TOKEN_F8: printf("F8"); break;
+            case TOKEN_B1: printf("B1"); break;
+            case TOKEN_V0: printf("V0"); break;
+
             case TOKEN_INT_LITERAL: printf("INT_LITERAL"); break;
             case TOKEN_FLOAT_LITERAL: printf("FLOAT_LITERAL"); break;
-            case TOKEN_CHAR_LITERAL: printf("CHAR_LITERAL"); break;
+            case TOKEN_C1_LITERAL: printf("C1_LITERAL"); break;
+            case TOKEN_C2_LITERAL: printf("C2_LITERAL"); break;
+            case TOKEN_C4_LITERAL: printf("C4_LITERAL"); break;
+            case TOKEN_S1_LITERAL: printf("S1_LITERAL"); break;
+            case TOKEN_S2_LITERAL: printf("S2_LITERAL"); break;
+            case TOKEN_S4_LITERAL: printf("S4_LITERAL"); break;
             case TOKEN_TRUE: printf("TRUE"); break;
             case TOKEN_FALSE: printf("FALSE"); break;
             case TOKEN_IDENTIFIER: printf("IDENTIFIER"); break;
